06cad1ee7dda1335f19d6a71c5cdea335df96a46
kimisaac1998 <kimisaac1998@gmail.com>
2022-06-10 14:12:51 -0700

    Initial commmit

---
+++ src/Components/StockDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StockDetails(props) {
+    return (
+        <div>
+            <h2>Stockdetails</h2>
+        </div>
+    );
+}
+
+export default StockDetails;
